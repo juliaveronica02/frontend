0b6a743e9859850aef9c3c7a9c50c8e42f69433b
ferdinandw <fwng123@gmail.com>
2020-05-24 17:47:48 +0700

    sign up

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -47,7 +47,7 @@ class Register extends Component {
       password: this.state.password,
       password2: this.state.password2,
     };
-    this.props.registerUser(newUser, this.props.history.push("/signin"));
+    this.props.registerUser(newUser);
     console.log(newUser);
   };
   render() {
