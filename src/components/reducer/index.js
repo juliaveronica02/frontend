4ec92067d5699b3cdd6af90c20a2c2f26e634bda
juliaveronica02 <juliavero2206@gmail.com>
2020-06-11 22:55:30 +0700

    navbar, signIn, signUp done

--- src/components/reducer/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "./auth";
-import errorReducer from "./error";
-// import cart from './CartReducer'
-// import item from './Item'
-export default combineReducers({ auth: authReducer, errors: errorReducer });
