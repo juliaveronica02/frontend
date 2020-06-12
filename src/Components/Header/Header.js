16209311beb63b0305057d58378f4b0c4371b4cb
juliaveronica02 <juliavero2206@gmail.com>
2020-06-12 10:10:33 +0700

    done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,67 +1,54 @@
 import React from "react";
 import Background from "../../img/shoes.webp";
 import Typed from "react-typed";
+import redShoes from "../../img/red-shoes.png";
+import "./navbar.scss";
+import Home from "../../Pages/Home/Home"
 
 function Header() {
   return (
-    <div>
-      <header style={headerStyle}>
-        <div className="container">
-          <div style={textStyle}>
-            {/* <center> */}
-            {/* <Typed
-                style={{
-                  size: 34,
-                  fontSize: "34px",
-                  fontFamily: "Barlow Semi Condensed",
-                }}
-                strings={["Trishop"]}
-                typeSpeed={40}
-                backSpeed={50}
-              />
-            </center>
-            <br /> */}
-            {/* <Typed
-              style={{
-                size: 24,
-                fontSize: "24px",
-                fontFamily: "Barlow Semi Condensed",
-              }}
-              strings={
-                [
-                  // "^1500",
-                  // "We provide development",
-                  // "We provide supply",
-                  // "We provide management",
-                ]
-              }
-              typeSpeed={50}
-              backSpeed={80}
-              backDelay={150}
-              loop
-            ></Typed> */}
-          </div>
-        </div>
-      </header>
-    </div>
+    <>
+     {/* Hero */}
+     <div id="hero">
+     <div className="hero-left">
+       <div className="hero-el">
+         <h1>Boost Up Your Style</h1>
+         <h1>A Happy Future.</h1>
+       </div>
+       <div className="hero-el">
+         <p>
+           Get ready for greatness, We only provide the best of the best
+           quality.
+         </p>
+       </div>
+       <div>
+         <button>Shop Now</button>
+       </div>
+     </div>
+     <div className="hero-right">
+       <img src={redShoes} alt="shoes1" />
+     </div>
+   </div>
+      <Home/>
+   </>
   );
 }
 
 export default Header;
-const headerStyle = {
-  backgroundImage: `url(${Background})`,
-  backgroundPosition: "center",
-  backgroundSize: "cover",
-  backgroundRepeat: "no-repeat",
-  display: "block",
-};
-const textStyle = {
-  minHeight: "70vh",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  color: "#fff",
-  fontSize: "18px",
-  fontWeight: 400,
-};
+// const headerStyle = {
+//   backgroundImage: `url(${Background})`,
+//   backgroundPosition: "center",
+//   backgroundSize: "cover",
+//   backgroundRepeat: "no-repeat",
+//   display: "block",
+// };
+// const textStyle = {
+//   minHeight: "70vh",
+//   display: "flex",
+//   flexDirection: "column",
+//   alignItems: "center",
+//   justifyContent: "center",
+//   color: "#fff",
+//   fontSize: "18px",
+//   fontWeight: 400,
+// };
