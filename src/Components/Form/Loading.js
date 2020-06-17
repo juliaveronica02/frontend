74ce10f00f25c854173ed88372fb41becdffc8f6
ferdinandw <fwng123@gmail.com>
2020-06-18 02:47:03 +0700

    loading spinner

---
+++ src/Components/Form/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {faSpinner} from '@fortawesome/free-solid-svg-icons'
+
+    const LoadingSpinner = () => (
+      <div>
+        <i className="fa fa-spinner fa-spin" /> Loading...
+      </div>
+    );
+
+export default LoadingSpinner;
